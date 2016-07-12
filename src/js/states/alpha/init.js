86e8c48578e9d26e58f2e099848e36a826b2b1ea
Duncan Kolba <duncankolba@gmail.com>
2016-07-12 21:46:23 +0200

    Update to Phase.js to 2.6

--- src/js/states/alpha/init.js
+++ src/js/states/alpha/init.js
@@ -2,10 +2,10 @@ const minion = require('../../lib/minion');
 
 function init() {
   const box = minion.drawRect(this);
-  const spinner = this.add.sprite(this.world.centerX, this.world.centerY,
+  this.spinner = this.add.sprite(this.world.centerX, this.world.centerY,
       box.generateTexture());
 
-  spinner.anchor.set(0.5);
+  this.spinner.anchor.set(0.5);
 }
 
 module.exports = init;
